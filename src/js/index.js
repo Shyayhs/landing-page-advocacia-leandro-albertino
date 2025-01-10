db0f7a49e09becdf8229f9a37b4d2923f8dce627
Pedro Lucas <pedrolucasdemoura@gmail.com>
2025-01-10 18:55:47 -0300

    Adição do código em js para ocultar as descrições dos serviços, e do botão "Entre em contato"

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 AOS.init();
+
+const buttons = document.querySelectorAll(".know-more");
+const descriptions = document.getElementsByClassName("service-description");
+const serviceItems = document.getElementsByClassName("service-item");
+
+buttons.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        let description = descriptions[index];
+        let serviceItem = serviceItems[index];
+
+        if (description.classList.contains("visible") === true) {
+            description.classList.remove("visible");
+            serviceItem.classList.remove("on");
+            
+            element.innerHTML = "Saiba mais"
+        } 
+        else {
+            description.classList.add("visible");
+            serviceItem.classList.add("on");
+
+           element.innerHTML = "Fechar"
+        }
+    });
+});
